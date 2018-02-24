d38e501a8357d4e873acbd3792b320f1ecf9e244
Liyao <Liyao_0419@outlook.com>
2018-02-24 14:03:39 +0000

    Add files via upload

--- plot_penalty_effect.py
+++ plot_penalty_effect.py
@@ -20,11 +20,11 @@ patterns.append(re.compile("penalty\s*=\s*(.*)$"))
 patterns.append(re.compile("SENT:\s%Correct=(\d+\.\d+)\s\[.*\]$"))
 patterns.append(re.compile("WORD:\s%Corr=(\d+\.\d+),\s\S+\s\[\S+,\sD=(\d+),\s\S+\sI=(\d+),\s\S+\]"))
 
-penalty = []
-sent_corr = []
-word_corr = []
-deletion_num = []
-insertion_num = []
+penalty = []         #penalty values list
+sent_corr = []       #sentence correctness rate list
+word_corr = []       #word correctness rate list
+deletion_num = []    #deletion number list
+insertion_num = []   #insertion number list
 
 #Open file
 try:
@@ -42,15 +42,15 @@ while True:
         print("File process complete!")
         break;
 
-    #Match penalty line
+    #Extract penalty value
     m = patterns[0].search(line_penalty)
     penalty.append(int(m.group(1)))
     
-    #Match sentence correctness line
+    #Extract sentence correctness rate
     m = patterns[1].search(line_sent)
     sent_corr.append(float(m.group(1)))
 
-    #Match word correctness line
+    #Extract word correctness rate,  deletion error, insertion error
     m = patterns[2].search(line_word)
     word_corr.append(float(m.group(1)))
     deletion_num.append(int(m.group(2)))
@@ -60,21 +60,22 @@ fd.close()
 
 #Plot 4 graphs
 fig, ((plt_sent_corr, plt_word_corr), (plt_deletion_num, plt_insertion_num)) = plt.subplots(2, 2)
+#plot sentence correctness rate
 plt_sent_corr.plot(penalty, sent_corr)
 plt_sent_corr.set_title("Sentence correctness")
 plt_sent_corr.set_xlabel("penalty")
 plt_sent_corr.set_ylabel("correct(%)")
-
+#plot word correcteness rate
 plt_word_corr.plot(penalty, word_corr)
 plt_word_corr.set_title("Word correctness")
 plt_word_corr.set_xlabel("penalty")
 plt_word_corr.set_ylabel("correct(%)")
-
+#deletion number
 plt_deletion_num.plot(penalty, deletion_num)
 plt_deletion_num.set_title("Number of deletion")
 plt_deletion_num.set_xlabel("penalty")
 plt_deletion_num.set_ylabel("number")
-
+#insertion number
 plt_insertion_num.plot(penalty, insertion_num)
 plt_insertion_num.set_title("Number of insertion")
 plt_insertion_num.set_xlabel("penalty")
